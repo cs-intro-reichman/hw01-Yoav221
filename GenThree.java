1d8abda4c2dfab016a55483bbcf27318453579da
Yoav Damari <yoav.damari@runi.ac.il>
2023-12-18 21:49:27 +0200

    s

--- GenThree.java
+++ GenThree.java
@@ -4,16 +4,15 @@ public class GenThree {
 	public static void main(String[] args) {
 
 		int a = Integer.parseInt(args[0]);
-
 		int b = Integer.parseInt(args[1]);
 
 		int random_int_1 = ThreadLocalRandom.current().nextInt(a, b);
 		int random_int_2 = ThreadLocalRandom.current().nextInt(a, b);
 		int random_int_3 = ThreadLocalRandom.current().nextInt(a, b);
 
-		// System.out.println(random_int_1);
-		// System.out.println(random_int_2);
-		// System.out.println(random_int_3);
+		System.out.println(random_int_1);
+		System.out.println(random_int_2);
+		System.out.println(random_int_3);
 
 		System.out.println(Math.min(Math.min(random_int_1, random_int_2), random_int_3));
 
