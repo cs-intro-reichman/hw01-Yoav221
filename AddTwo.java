75b6270c5b323e38154f4c292ee3eae750cdf07a
Yoav Damari <yoav.damari@runi.ac.il>
2023-12-18 14:46:13 +0200

    first code

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,14 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		int first_num = 2;
+		int second_num = 2;
+
+		int sum = first_num + second_num;
+
+		System.out.println(sum);
+		System.out.println("Done!");
+
+		// Put your code here
 	}
 }
