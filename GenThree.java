0ba02e749472496d35c839085e2d0169a5d6513a
Yoav Damari <yoav.damari@runi.ac.il>
2023-12-18 19:52:25 +0200

    aa

--- GenThree.java
+++ GenThree.java
@@ -4,21 +4,19 @@ import java.util.Scanner;
 public class GenThree {
 	public static void main(String[] args) {
 
-		
-			int a = Integer.parseInt(args[0]);
-			
-			int b = Integer.parseInt(args[1]);
+		int a = Integer.parseInt(args[0]);
 
-			int random_int_1 = ThreadLocalRandom.current().nextInt(a, b + 1);
-			int random_int_2 = ThreadLocalRandom.current().nextInt(a, b + 1);
-			int random_int_3 = ThreadLocalRandom.current().nextInt(a, b + 1);
+		int b = Integer.parseInt(args[1]);
 
-			System.out.println(random_int_1);
-			System.out.println(random_int_2);
-			System.out.println(random_int_3);
+		int random_int_1 = ThreadLocalRandom.current().nextInt(a, b + 1);
+		int random_int_2 = ThreadLocalRandom.current().nextInt(a, b + 1);
+		int random_int_3 = ThreadLocalRandom.current().nextInt(a, b + 1);
 
-			System.out.println("Minimal number is: " + Math.min(Math.min(random_int_1, random_int_2), random_int_3));
-		}
+		System.out.println(random_int_1);
+		System.out.println(random_int_2);
+		System.out.println(random_int_3);
+
+		System.out.println("Minimal number is: " + Math.min(Math.min(random_int_1, random_int_2), random_int_3));
 
 	}
 }
