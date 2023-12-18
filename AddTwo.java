e0b3487cf8728782169046c84e2a9700be79cecf
Yoav Damari <yoav.damari@runi.ac.il>
2023-12-18 16:39:50 +0200

    finishing addtwo

--- AddTwo.java
+++ AddTwo.java
@@ -1,12 +1,23 @@
+
 /*
  * Adds two given integers and prints the result in a fancy way.
  */
+import java.util.Scanner;
+
 public class AddTwo {
 	public static void main(String[] args) {
-		int first_num = 2;
-		int second_num = 2;
 
-		System.out.println(first_num + second_num);
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("First integer: ");
+		int first_int = scanner.nextInt();
+
+		System.out.println("Second integer: ");
+		int second_int = scanner.nextInt();
+
+		int sum = first_int + second_int;
+
+		System.out.println("The sum is: " + sum);
 
 	}
 }
