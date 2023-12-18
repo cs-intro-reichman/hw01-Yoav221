5b5dce2aaf79858af16d3e5e0da8358495341608
Yoav Damari <yoav.damari@runi.ac.il>
2023-12-18 20:13:27 +0200

    genthree

--- GenThree.java
+++ GenThree.java
@@ -11,9 +11,9 @@ public class GenThree {
 		int random_int_2 = ThreadLocalRandom.current().nextInt(a, b);
 		int random_int_3 = ThreadLocalRandom.current().nextInt(a, b);
 
-		System.out.println(random_int_1);
-		System.out.println(random_int_2);
-		System.out.println(random_int_3);
+		// System.out.println(random_int_1);
+		// System.out.println(random_int_2);
+		// System.out.println(random_int_3);
 
 		System.out.println(Math.min(Math.min(random_int_1, random_int_2), random_int_3));
 
