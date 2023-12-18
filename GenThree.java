b5e785ff6279bad97763c57b611499360df7de03
Yoav Damari <yoav.damari@runi.ac.il>
2023-12-18 19:52:55 +0200

    nothing special

--- GenThree.java
+++ GenThree.java
@@ -1,5 +1,4 @@
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;;
 
 public class GenThree {
 	public static void main(String[] args) {
