e2d2faf8ed3c02c3511ede45d96488cdf33e60b1
Yoav Damari <yoav.damari@runi.ac.il>
2023-12-18 17:28:55 +0200

    finishing coins

--- AddTwo.java
+++ AddTwo.java
@@ -7,17 +7,17 @@ import java.util.Scanner;
 public class AddTwo {
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("First integer: ");
+			int first_int = scanner.nextInt();
 
-		System.out.println("First integer: ");
-		int first_int = scanner.nextInt();
+			System.out.println("Second integer: ");
+			int second_int = scanner.nextInt();
 
-		System.out.println("Second integer: ");
-		int second_int = scanner.nextInt();
+			int sum = first_int + second_int;
 
-		int sum = first_int + second_int;
-
-		System.out.println("The sum is: " + sum);
+			System.out.println("The sum is: " + sum);
+		}
 
 	}
 }
--- Coins.java
+++ Coins.java
@@ -1,9 +1,24 @@
+
 /*
  * Write a program that gets a quantity of cents as a command-line argument.
  * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
  */
+import java.util.Scanner;
+
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		try (// Put your code here
+				Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Quantity of cents: ");
+			int cents = scanner.nextInt();
+
+			int quarters = cents / 25;
+			int reminder = cents % 25;
+
+			System.out.println("Quarters: " + quarters);
+			System.out.println("Reminder in cents: " + reminder);
+
+		}
+
 	}
 }
