b6073192465870ec483f9eed67a4f427aaf88482
Yoav Damari <yoav.damari@runi.ac.il>
2023-12-20 13:43:50 +0200

    finishing linear

--- LinearEq.java
+++ LinearEq.java
@@ -6,9 +6,14 @@
  */
 public class LinearEq {
 	public static void main(String[] args) {
-		int a = Integer.parseInt(args[0]);
-		int b = Integer.parseInt(args[1]);
-		int c = Integer.parseInt(args[2]);
+		double a = Double.parseDouble(args[0]);
+		double b = Double.parseDouble(args[1]);
+		double c = Double.parseDouble(args[2]);
+
+		double x = (c - b) / a;
+		System.out.println(a + " * " + "x" + " + " + b + " = " + c);
+
+		System.out.println("x = " + x);
 
 	}
 }
